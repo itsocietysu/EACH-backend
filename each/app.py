46c85d48fb1a6ee81d2d7ff75d9973e255ec5a66
leins <feitrich@yandex.ru>
2018-07-26 12:22:00 +0300

    Added correct json to return in feed functions

--- each/app.py
+++ each/app.py
@@ -271,7 +271,14 @@ def addFeed(**request_handler_args):
         if id:
             objects = EntityNews.get().filter_by(eid=id).all()
 
-            resp.body = obj_to_json([o.to_dict() for o in objects])
+            res = []
+            for _ in objects:
+                obj_dict = _.to_dict(['eid', 'title', 'text'])
+                wide_info = EntityNews.get_wide_object(_.eid, ['image'])
+                obj_dict.update(wide_info)
+                res.append(obj_dict)
+
+            resp.body = obj_to_json(res)
             resp.status = falcon.HTTP_200
             return
     except ValueError:
@@ -293,7 +300,14 @@ def updateFeed(**request_handler_args):
         if id:
             objects = EntityNews.get().filter_by(eid=id).all()
 
-            resp.body = obj_to_json([o.to_dict() for o in objects])
+            res = []
+            for _ in objects:
+                obj_dict = _.to_dict(['eid', 'title', 'text'])
+                wide_info = EntityNews.get_wide_object(_.eid, ['image'])
+                obj_dict.update(wide_info)
+                res.append(obj_dict)
+
+            resp.body = obj_to_json(res)
             resp.status = falcon.HTTP_200
             return
     except ValueError:
@@ -343,7 +357,7 @@ def deleteFeed(**request_handler_args):
     req = request_handler_args['req']
 
     id = getIntPathParam("feedId", **request_handler_args)
-
+    res = []
     try:
         EntityNews.delete(id)
     except FileNotFoundError:
@@ -358,6 +372,7 @@ def deleteFeed(**request_handler_args):
 
     object = EntityNews.get().filter_by(eid=id).all()
     if not len(object):
+        resp.body = obj_to_json(res)
         resp.status = falcon.HTTP_200
         return
 
